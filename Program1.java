37eda75a6226b43549e0fbe635196bca5d85d25e
Shyam James <shyamjames74@gmail.com>
2025-11-18 18:14:04 +0530

    Program 1

---
+++ Program1.java
@@ -0,0 +1,16 @@
+/*
+ * Write a program to perform the mathematical operations using switch case.
+ * @Shyam James
+ * Roll No 54
+ * 17/11/2025
+ */
+import java.io.*;
+
+class Program1{
+	public static void main(String args[]){
+		String a = args[0];
+		String b = args[1];
+		System.out.println(a);
+		System.out.println(b);
+	}	
+}
