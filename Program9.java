ffa8015f04c96f591ec11d77bd7a81720daeebb8
Shyam James <shyamjames74@gmail.com>
2025-12-08 00:06:53 +0530

    program 9

--- Program9.java
+++ Program9.java
@@ -14,13 +14,116 @@ Roll No: 54
 
 import java.util.*;
 
-class Field{
-    public void printGrid(int a[][]){
-        for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
-                System.out.println(a[i][j]+" ");
-            }
-            System.out.println("");
+class Puzzle {
+    int[] a;
+    int x;
+    int count;
+
+    Puzzle() {
+        count = 0;
+        a = new int[16];
+        
+        for (int i = 0; i < 15; i++) {
+            a[i] = i + 1;
         }
+        a[15] = 0;
+        
+        Random r = new Random();
+        for (int i = 0; i < 15; i++) {
+            int j = r.nextInt(15);
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+        x = 15;
+    }
+
+    void draw() {
+        for (int i = 0; i < 16; i++) {
+            if (a[i] == 0) {
+                System.out.printf("%-4c", '_');
+            } else {
+                System.out.printf("%-4d", a[i]);
+            }
+            
+            if ((i + 1) % 4 == 0) {
+                System.out.println();
+            }
+        }
+    }
+
+    boolean won() {
+        for (int i = 0; i < 15; i++) {
+            if (a[i] != i + 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void move() {
+        Scanner s = new Scanner(System.in);
+        char dir;
+        int temp;
+
+        while (true) {
+            draw();
+            System.out.println("Click [w][a][s][d] to move:");
+            dir = s.next().charAt(0);
+            count++;
+
+            if (dir == 'W' || dir == 'w') {
+                if ((x - 4) < 0) {
+                    System.out.println("Unable to move up!!");
+                } else {
+                    temp = a[x];
+                    a[x] = a[x - 4];
+                    a[x - 4] = temp;
+                    x = x - 4;
+                }
+            } 
+            else if (dir == 'A' || dir == 'a') {
+                if (x % 4 == 0) {
+                    System.out.println("Unable to move left!!");
+                } else {
+                    temp = a[x];
+                    a[x] = a[x - 1];
+                    a[x - 1] = temp;
+                    x = x - 1;
+                }
+            } 
+            else if (dir == 'S' || dir == 's') {
+                if ((x + 4) > 15) {
+                    System.out.println("Unable to move down!!");
+                } else {
+                    temp = a[x];
+                    a[x] = a[x + 4];
+                    a[x + 4] = temp;
+                    x = x + 4;
+                }
+            } 
+            else if (dir == 'D' || dir == 'd') {
+                if ((x + 1) % 4 == 0) {
+                    System.out.println("Unable to move right!!");
+                } else {
+                    temp = a[x];
+                    a[x] = a[x + 1];
+                    a[x + 1] = temp;
+                    x = x + 1;
+                }
+            }
+
+            if (won()) {
+                System.out.println("Congrats You Won in " + count + " moves");
+                break;
+            }
+        }
+    }
+}
+
+public class Program9 {
+    public static void main(String[] args) {
+        Puzzle obj = new Puzzle();
+        obj.move();
     }
 }
