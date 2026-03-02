c7dbaf879f7144551cb2a0e65d8da774017d5898
Shyam James <shyamjames74@gmail.com>
2026-03-02 22:13:28 +0530

    Program 41

---
+++ Program41Client.java
@@ -0,0 +1,42 @@
+/* Program to implement one to one chatting.
+--- Client Program ---
+@Shyam James
+Roll no: 54
+Date: 12-01-2026
+*/
+
+import java.io.*;
+import java.net.*;
+
+public class Program41Client{
+    public static void main(String[] args) throws Exception {
+
+        Socket s = new Socket("localhost", 5000);
+        System.out.println("Connected to Server!");
+
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String msg = "", reply = "";
+
+        while (!msg.equals("exit")) {
+
+            // Send message
+            System.out.print("You: ");
+            msg = br.readLine();
+            dos.writeUTF(msg);
+
+            if (msg.equals("exit"))
+                break;
+
+            // Receive reply
+            reply = dis.readUTF();
+            System.out.println("Server: " + reply);
+        }
+
+        dis.close();
+        dos.close();
+        s.close();
+    }
+}
---
+++ Program41Server.java
@@ -0,0 +1,45 @@
+/* Program to implement one to one chatting.
+--- Server Program ---
+@Shyam James
+Roll no: 54
+Date: 12-01-2026
+*/
+
+import java.io.*;
+import java.net.*;
+
+class Program41Server{
+    public static void main(String[] args) {
+        try{
+            ServerSocket ss = new ServerSocket(5000);
+            System.out.println("Server waiting for client...");
+
+            Socket s = ss.accept();
+            System.out.println("Client Connected!");
+
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String msg = "", reply = "";
+
+            while (!msg.equals("exit")) {
+                // Receive from client
+                msg = dis.readUTF();
+                System.out.println("Client: " + msg);
+                if (msg.equals("exit"))
+                    break;
+                // Send reply
+                System.out.print("You: ");
+                reply = br.readLine();
+                dos.writeUTF(reply);
+            }
+            dis.close();
+            dos.close();
+            s.close();
+            ss.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
